123fcf8b650e8625193955b8eeaac7996955f549
Theertha K <theerthak964@gmail.com>
2023-10-08 21:10:17 +0530

    hospital management project

---
+++ myapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from myapp.models import Deparetment,Docter,Booking
+
+
+admin.site.register(Deparetment)
+admin.site.register(Docter)
+
+class Bookingadmin(admin.ModelAdmin):
+    list_display=('id','p_name','p_email','p_phone','doc_name','booking_date','bookedon')
+
+
+admin.site.register(Booking,Bookingadmin)
